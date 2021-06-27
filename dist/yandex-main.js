31c5cb2a0a4fc5e966915ba17737df24ec683b62
veu <pika-piba@mail.ru>
2021-06-27 23:04:20 +0300

    finished mail

--- dist/yandex-main.js
+++ dist/yandex-main.js
@@ -1,15 +1,33 @@
+
 const input = document.querySelector(".bar-form__input");
-const actions = document.querySelector("input ~ div");
+const microIcon = document.querySelector(".micro-icon");
+const keyboardIcon = document.querySelector(".keyboard-icon");
+const closeIcon = document.querySelector(".close-icon");
 
 input.addEventListener("focus", function () {
-    actions.style.opacity = "0";
+    keyboardIcon.style.opacity = "0";
+    microIcon.style.opacity = "0";
+    closeIcon.style.opacity = "1";
+})
+
+if (input.value.length > 100) {
+    closeIcon.style.opacity = "0";
+}
+
+input.addEventListener("blur", function () {
+    if (input.value.length > 0) {
+        keyboardIcon.style.opacity = "0";
+        microIcon.style.opacity = "0";
+
+    } else {
+        keyboardIcon.style.opacity = "1";
+        microIcon.style.opacity = "1";
+        closeIcon.style.opacity = "0";
+    }
 })
 
 input.addEventListener("blur", function () {
-    if (input.value.length > 5) {
-        console.log(input.value.length);
-        actions.style.opacity = "0";
-    } else {
-        actions.style.opacity = "1";
+    if (input.value.length > 100) {
+        closeIcon.style.opacity = "0";
     }
 })
