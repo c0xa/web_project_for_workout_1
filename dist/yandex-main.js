080d95ff2e2fffe106abce90fc095f9c4286e3b3
veu <pika-piba@mail.ru>
2021-07-05 16:12:07 +0300

    started css-tricks

--- dist/yandex-main.js
+++ dist/yandex-main.js
@@ -1,5 +1,37 @@
+const notification = document.querySelector(".ya-notification__covid-histogram");
+const notificationCovid = document.querySelector(".ya-covid-histogram");
+const close = document.querySelector(".ya-covid-histogram__icon.close-icon");
 
-const input = document.querySelector(".bar-form__input");
+function mouseover() {
+    notificationCovid.classList.add("_move");
+    close.classList.add("_view");
+}
+
+function mouseout() {
+    notificationCovid.classList.remove("_move");
+    close.classList.remove("_view");
+}
+
+notification.addEventListener("mouseover", mouseover)
+notification.addEventListener("mouseout", mouseout)
+
+notificationCovid.addEventListener("click", function() {
+    notification.removeEventListener("mouseover", mouseover);
+    notification.removeEventListener("mouseout", mouseout);
+
+    notificationCovid.classList.add("_move");
+    close.classList.add("_view");
+})
+
+close.addEventListener("click", function() {
+    notificationCovid.classList.remove("_move");
+    close.classList.remove("_view");
+
+    notification.addEventListener("mouseover", mouseover);
+    notification.addEventListener("mouseout", mouseout);
+})
+
+const input = document.querySelector(".ya-bar-form__input");
 const microIcon = document.querySelector(".micro-icon");
 const keyboardIcon = document.querySelector(".keyboard-icon");
 const closeIcon = document.querySelector(".close-icon");
@@ -9,10 +41,6 @@ input.addEventListener("focus", function () {
     microIcon.style.opacity = "0";
     closeIcon.style.opacity = "1";
 })
-
-if (input.value.length > 100) {
-    closeIcon.style.opacity = "0";
-}
 
 input.addEventListener("blur", function () {
     if (input.value.length > 0) {
--- gulpfile.js
+++ gulpfile.js
@@ -47,9 +47,15 @@ gulp.task("svgstore", function () {
 			.pipe(gulp.dest("./src")),
 
 		gulp
+			.src("./src/css-tricks-index.html")
+			.pipe(inject(svgs, {transform: fileContents}))
+			.pipe(gulp.dest("./src")),
+
+		gulp
 			.src("./src/mail-index.html")
 			.pipe(inject(svgs, {transform: fileContents}))
 			.pipe(gulp.dest("./src")));
+	
 });
 
 gulp.task("less", function () {
@@ -72,6 +78,11 @@ gulp.task("html", function () {
 		gulp
 			.src("./src/yandex-index.html")
 			.pipe(gulp.dest("./dist")),
+
+		gulp
+			.src("./src/css-tricks-index.html")
+			.pipe(gulp.dest("./dist")),
+
 
 		gulp
 			.src("./src/mail-index.html")
@@ -105,12 +116,14 @@ gulp.task("serve", function () {
 	gulp.watch("./src/index.html").on("change", series("html"));
 	gulp.watch("./src/yandex-index.html").on("change", series("html"));
 	gulp.watch("./src/mail-index.html").on("change", series("html"));
+	gulp.watch("./src/css-tricks-index.html").on("change", series("html"));
 	gulp.watch("./src/assets/js/**/*.js").on("change", series("js"));
 
 	gulp.watch("./dist/main.css").on("change", browserSync.reload);
 	gulp.watch("./dist/index.html").on("change", browserSync.reload);
 	gulp.watch("./dist/yandex-index.html").on("change", browserSync.reload);
 	gulp.watch("./dist/yandex-main.js").on("change", browserSync.reload);
+	gulp.watch("./dist/css-tricks-index.js").on("change", browserSync.reload);
 	gulp.watch("./dist/mail-index.html").on("change", browserSync.reload);
 });
 
